a0b975a8ae2cfb1bdb40a78893fa271703cbb1ef
NatinaelSeifu <59883206+NatinaelSeifu@users.noreply.github.com>
2022-04-02 00:02:12 +0100

    static libraries

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,30 +1,27 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- * *_strstr - description
- * @haystack: string
- * @needle: pointer
- * Return: pointer
+ * *_strstr - locates a substring
+ * @haystack: string to search in
+ * @needle: substring to look for
+ *
+ * Return: pointer to the beginning of the located substring
+ * or NULL if the substring is not found
  */
-
-
 char *_strstr(char *haystack, char *needle)
 {
-int i, j;
+	int i, j;
 
-for (i = 0; haystack[i] > '\0'; i++)
-{
-	for (j = i; haystack[j] > '\0' && needle[j - i] > '\0'; j++)
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		if (haystack[j] != needle[j - i])
+		for (j = 0; needle[j] != '\0'; j++)
 		{
-			break;
+			if (haystack[i + j] != needle[j])
+				break;
 		}
+		if (!needle[j])
+			return (&haystack[i]);
 	}
-	if (needle[j - i] == '\0')
-	{
-		return (haystack + i);
-	}
-}
-return (0);
+	return (NULL);
 }
--- 0x09-static_libraries/README.md
+++ 0x09-static_libraries/README.md
@@ -1,3 +1,1 @@
-<h1>Introduction to C - Static libraries  
-<h3>Create the static library libmy.a containing all the functions listed below  
-<h3>Create a script called create_static_lib.sh that creates a static library called liball.a from all the .c files that are in the current directory.  
+# C - Static libraries
